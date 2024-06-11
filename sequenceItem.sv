0ac537c062a5e9d96af8112aa93ed70dff1121c5
taher hasan <112001968+taherhasan2001@users.noreply.github.com>
2024-06-11 22:08:43 +0300

    Update sequenceItem.sv

--- sequenceItem.sv
+++ sequenceItem.sv
@@ -1,43 +1,13 @@
-
-//------------------------------------------------------------------------
-//				Memory Model RTL - www.verificationguide.com
-//------------------------------------------------------------------------
-/*
-              		  -----------------
-            		 |                |
-    				 |                |
-        command ---->|                |------> compressed_out
-          		     |    			  |
-   		  			 |                |
-              		 |                |
-        data_in ---->| Final Project  |------> decompressed_out
-    				 |                |
-              		 |                |
-               		 |    			  |
-  compressed_in ---->|                |------> response
-                     |                |
-              		 |                |
-              		 -----------------
-                   		^        ^
-                   		|  	     |
-                	   clk     reset
-
-*/
-
-
-
-
-
 class comp_decomp_seq_item extends uvm_sequence_item;
   
-  rand bit 		 	reset, 
-  rand bit [79:0] 	data_in,
-  rand bit [7:0] 	compressed_in,
-  rand bit [1:0] 	command,
+  rand logic 		 	reset, 
+  rand logic [79:0] 	data_in,
+  rand logic [7:0] 	compressed_in,
+  rand logic [1:0] 	command,
   
-       bit [7:0] 	compressed_out, 
-  	   bit [79:0] 	decompressed_out,
-  	   bit [1:0] 	response,
+       logic [7:0] 	compressed_out, 
+  	   logic [79:0] 	decompressed_out,
+  	   logic [1:0] 	response,
   
   //Utility and Field macros,
   `uvm_object_utils_begin(comp_decomp_seq_item)
